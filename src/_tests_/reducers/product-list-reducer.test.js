6b0aa53b34436b3947176779c3c2e7a7d5375b51
Thomas Glenn <thomasgrantglenn@mac.com>
2020-09-18 09:57:58 -0600

    add first failed test of DELETE PRODUCT action.:

--- src/_tests_/reducers/product-list-reducer.test.js
+++ src/_tests_/reducers/product-list-reducer.test.js
@@ -2,11 +2,31 @@ import productListReducer from '../../reducers/product-list-reducer';
 
 describe('productListReducer', () => {
 
+  const currentState = {
+    1: {
+      name: 'Fantasy Island',
+      brand: 'Heavenly Bootch',
+      flavor: 'Ginger',
+      price: '8.00',
+      pints: '124',
+      id: 1
+    },
+    2: {
+      name: 'Tummy Tamer',
+      brand: 'Heavenly Bootch',
+      flavor: 'Grapefruit',
+      price: '8.99',
+      pints: '123',
+      id: 2
+    }
+  }
+
+
   let action;
   const productInfo = {
     name: 'Fantasy Island',
     brand: 'Heavenly Bootch',
-    flavor: 'Ginger Balls',
+    flavor: 'Ginger',
     price: '8.00',
     pints: '124',
     id: 1
@@ -23,9 +43,9 @@ describe('productListReducer', () => {
       pints: pints,
       id: id
     };
-  
+
     expect(productListReducer({}, action)).toEqual({
-      [id] : {
+      [id]: {
         name: name,
         brand: brand,
         flavor: flavor,
@@ -37,6 +57,24 @@ describe('productListReducer', () => {
   });
 
   test('Should return default state if there is no action passed into the reducer', () => {
-    expect(productListReducer({}, { type: null})).toEqual({});
+    expect(productListReducer({}, { type: null })).toEqual({});
   });
+
+  test('Should successfully delete a product', () => {
+    action = {
+      type: 'DELETE_PRODUCT',
+      id: 1
+    };
+    expect(productListReducer(currentState, action)).toEqual({
+      2: {
+        name: 'Tummy Tamer',
+        brand: 'Heavenly Bootch',
+        flavor: 'Grapefruit',
+        price: '8.99',
+        pints: '123',
+        id: 2
+      }
+    });
+  });
+
 });
