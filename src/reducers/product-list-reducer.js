14421fef36a5427ce1b2f21b98cd826c2d4ef208
Thomas Glenn <thomasgrantglenn@mac.com>
2020-09-18 10:02:14 -0600

    add first successful test of DELETE PRODUCT action.

--- src/reducers/product-list-reducer.js
+++ src/reducers/product-list-reducer.js
@@ -12,6 +12,10 @@ export default (state = {}, action) => {
           id: id
         }
       });
+    case 'DELETE_PRODUCT':
+      const newState = { ...state };
+      delete newState[id];
+      return newState;
   default:
       return state;
   } 
