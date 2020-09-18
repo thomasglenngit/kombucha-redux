b3326d5ba52a84195075d358d3cce83de9fba62d
Thomas Glenn <thomasgrantglenn@mac.com>
2020-09-18 09:43:43 -0600

    add action type ADD PRODUCT to product list reducer.

--- src/reducers/product-list-reducer.js
+++ src/reducers/product-list-reducer.js
@@ -1,3 +1,18 @@
 export default (state = {}, action) => {
-  return state;
+  const { name, brand, flavor, price, pints, id } = action;
+  switch (action.type) {
+    case 'ADD_TICKET':
+      return Object.assign({}, state, {
+        [id]: {
+          name: name,
+          brand: brand,
+          flavor: flavor,
+          price: price,
+          pints: pints,
+          id: id
+        }
+      });
+  default:
+      return state;
+  } 
 };
