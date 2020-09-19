b8407fa7a4fef266b9081fe875008def0268fd7b
Thomas Glenn <thomasgrantglenn@mac.com>
2020-09-19 14:53:48 -0600

    add passing test of toggle form action.

--- src/_tests_/reducers/current-page-reducer.test.js
+++ src/_tests_/reducers/current-page-reducer.test.js
@@ -5,4 +5,8 @@ describe("currentPageReducer", () => {
   test('Shoud return default state if not action type is recognized', () => {
     expect(currentPageReducer(false, { type: null })).toEqual(false);
   });
+
+  test('Should toggle currentPage state between true and false', () => {
+    expect(currentPageReducer(false, { type: 'TOGGLE_FORM' })).toEqual(true);
+  });
 });
