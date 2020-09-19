9c1af23f917cb2b2b4e3fcbb8004b344c145132e
Thomas Glenn <thomasgrantglenn@mac.com>
2020-09-19 15:47:07 -0600

    add passing tests for rootreducer: current page, and master list.

--- src/_tests_/reducers/index-reducer.test.js
+++ src/_tests_/reducers/index-reducer.test.js
@@ -1,11 +1,24 @@
 import rootReducer from '../../reducers/index';
+import { createStore } from 'redux';
+import currentPageReducer from '../../reducers/current-page-reducer';
+import productListReducer from '../../reducers/product-list-reducer';
+
+let store = createStore(rootReducer);
 
 describe("rootReducer", () => {
 
   test('Should return default state if no action type is recognized', () => {
     expect(rootReducer({}, { type: null })).toEqual({
-      masterProductList: {},
+      masterList: {},
       currentPage: false
     });
+  });
+
+  test('Check that initial state of productListReducer matches root reducer', () => {
+    expect(store.getState().masterList).toEqual(productListReducer(undefined, { type: null}));
+  });
+
+  test('Check that initial state of currentPage matches root reducer', () => {
+    expect(store.getState().currentPage).toEqual(currentPageReducer(undefined, { type: null}));
   });
 });
