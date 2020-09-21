5a437a8c6300c7501193fbf4eadc567befd8168e
Thomas Glenn <thomasgrantglenn@mac.com>
2020-09-20 20:13:46 -0600

    add failed test of toggle form action.

--- src/_tests_/actions/index.test.js
+++ src/_tests_/actions/index.test.js
@@ -5,6 +5,11 @@ describe('kombucha actions', () => {
     expect(actions.deleteProduct(1)).toEqual({
       type: 'DELETE_PRODUCT', 
       id: 1
-    })
-  })
-})
+    });
+  });
+  it('toggleForm should create TOGGLE_FORM action', () => {
+    expect(actions.toggleForm()).toEqual({
+      type: 'TOGGLE_FORM'
+    });
+  });
+});
